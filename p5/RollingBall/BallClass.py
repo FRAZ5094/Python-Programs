9f402817980e9dc24eb2e3b41dc8a398572c0832
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-04-20 17:46:37 +0100

    Rolling ball progress

--- p5/RollingBall/BallClass.py
+++ p5/RollingBall/BallClass.py
@@ -1,13 +1,42 @@
 import p5
+import numpy as np
 
 class Ball:
 
-    def __init__(self,x,y,r):
+    def __init__(self,x,y,r,v):
         self.pos=p5.Vector(x,y)
+        self.vel=p5.Vector(v[0],v[1])
         self.r=r
-        self.d=r*2
+        self.gravity=p5.Vector(0,1)
+        self.g=1
 
+        
     def show(self):
-        p5.circle((self.pos.x,self.pos.y),self.d)
+        p5.circle((self.pos.x,self.pos.y),self.r*2)
 
+    def relativePos(self,other):
+        return other.pos-self.pos
 
+    def updatePos(self,other):
+        if self.touching:
+            angle=self.relativePos(other).angle_between(p5.Vector(0,1))
+            self.acc=self.g*np.sin(angle)
+            self.vel+=self.acc
+        else:
+            self.vel+=self.gravity
+
+        self.pos+=self.vel
+
+    def collision(self,other):
+        rel=self.relativePos(other)
+        mag=abs(rel)
+
+        if mag<=self.r+other.r:
+            self.touching=True
+            distanceInside=other.r+self.r-mag
+            rel.normalize()
+            rel*=-1
+            rel*=distanceInside
+            self.pos+=rel
+        else:
+            self.touching=False
--- p5/RollingBall/BallcantgoinBall.py
+++ p5/RollingBall/BallcantgoinBall.py
@@ -1,22 +1,23 @@
 import p5
+import numpy as np
 from BallClass import *
-
 def setup():
     global smallBall,bigBall
     p5.size(500,500)
-    bigBall=Ball(width/2,height,200,[0,0])
-    smallBall=Ball(width/2,height-220,20,[1,0])
+    bigBall=Ball(width/2,height/2,200,[0,0])
+    smallBall=Ball(width/2,250,20,[0,0])
 
 def draw():
     global smallBall,bigBall
     p5.background(0)
+    MousePos=p5.Vector(mouse_x,mouse_y)
+    smallBall.pos=MousePos
     smallBall.collision(bigBall)
-    smallBall.updatePos(bigBall)
-
     smallBall.show()
     bigBall.show()
     p5.stroke(255,0,0)
     p5.line((smallBall.pos),(bigBall.pos))
+    p5.line(())
     p5.stroke(255)
-
+    print(np.rad2deg(smallBall.relativePos(bigBall).angle_between(p5.Vector(0,1))))
 p5.run()

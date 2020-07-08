29d186ed5564c62afdb5b498f8cec1171c6efe08
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-07-08 19:29:10 +0100

    Finished LSF scraper

--- Webscraping/Selenium/RedditLivestreamfail.py
+++ Webscraping/Selenium/RedditLivestreamfail.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-
+import time
 #driver=webdriver.Chrome(r"C:\Users\frase\Downloads\chromedriver.exe")
 
 def Open_URLS_in_tabs(URLS,FirstTab):
@@ -11,35 +11,56 @@ def Open_URLS_in_tabs(URLS,FirstTab):
     if FirstTab=="no":
         driver.get(URLS[0])
         URLS.pop(0)
+    driver.maximize_window()
     if len(URLS)>=1:
         for URL in URLS:
             driver.execute_script('''window.open("{}","_blank");'''.format(URL))
 
 
-URL="https://old.reddit.com/r/LivestreamFails/"
+URL="https://old.reddit.com/r/LivestreamFail/"
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/68.0.3618.191"}
-
+print("loading {}".format(URL))
+start=time.perf_counter()
 page=requests.get(URL,headers=headers)
 soup=BeautifulSoup(page.content,"lxml")
+end=time.perf_counter()
+print("loaded in {} seconds".format(round(end-start,2)))
 
-PostN=int(input("How many posts do you want to watch?\n"))
+
+PostN=int(input("How many Clips do you want to watch?\n"))
+
 
 Posts=soup.find_all("div",class_="top-matter")
-Posts=Posts[:PostN]
+Upvotes=soup.find_all("div",class_="score unvoted")
+
 ClipURLS=[]
 ClipTitles=[]
-for Post in Posts:
-    ClipURLS.append(Post.p.a["data-href-url"])
-    ClipTitles.append(Post.p.a.get_text())
+ClipUpvotes=[]
 
+for i,Post in enumerate(Posts):
+    if Post.p.a["href"][0]!="/":
+        ClipURLS.append(Post.p.a["href"])
+        ClipTitles.append(Post.p.a.get_text())
+        ClipUpvotes.append(Upvotes[i].get_text())
+
+ClipURLS=ClipURLS[:PostN]
+ClipTitles=ClipTitles[:PostN]
+ClipUpvotes=ClipUpvotes[:PostN]
 
 print("Clips:")
 print(" ")
 for i,ClipTitle in enumerate(ClipTitles):
-    print(str(i+1)+": "+ClipTitle)
+    print("{}:{} {}".format(i+1,ClipTitle,ClipUpvotes[i]))
 
-n=input("Open clips in Chrome? y/n\n")
+print(" ")
+n=input("Open clips? y/n\n")
+
 if n=="y":
+    ClipURLS.reverse()
+    start=time.perf_counter()
     Open_URLS_in_tabs(ClipURLS,"yes")
+    end=time.perf_counter()
+    print("{} Clips loaded in {} seconds".format(PostN,round(end-start,2)))
+    n=input("Press to Exit\n")
 
-n=input("Press to Exit\n")
+

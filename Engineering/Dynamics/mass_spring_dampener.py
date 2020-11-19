ef39ea1daa1e6fb079e24588ea963ab454da92a5
FRAZ5094 <60670235+FRAZ5094@users.noreply.github.com>
2020-11-19 19:35:52 +0000

    mass sping to check answers

--- Engineering/Dynamics/mass_spring_dampener.py
+++ Engineering/Dynamics/mass_spring_dampener.py
@@ -2,7 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-def mysystem(x,t):
+def fraserq2(x,t):
+    #x[1]=omega
+    #x[0]=theta
+    
+    dx1dt=x[1]
+    dx2dt=16-61*x[1]-210*x[0]
+
+    dxdt=[dx1dt,dx2dt]
+    return dxdt
+
+def scottq2(x,t):
     #x[1]=omega
     #x[0]=theta
     
@@ -12,23 +22,28 @@ def mysystem(x,t):
     dxdt=[dx1dt,dx2dt]
     return dxdt
 
+
 t_start=0
 t_end=2
 t_delta=0.00001
 
 t=np.arange(t_start,t_end+t_delta,t_delta)
 
-x_init=[0,14]
+x_init=[0,21]
 
-response=odeint(mysystem,x_init,t)
+response=odeint(fraserq2,x_init,t)
 
 theta=response[:,0]
 omega=response[:,1]
 
-#scott_theta=(7/6)+(1/12)*np.exp(-3*t)-(5/4)*np.exp(-t)
+A=0.05827243987
+B=36.03012628
+fraserq1_theta=A*np.exp(-12.3*t)*np.sin(B*t)
 
-plt.plot(t,theta)
-#plt.plot(t,scott_theta)
+scottq1_theta=(0.95/np.sqrt(20.56))*np.exp(-0.8*t)*np.sin(np.sqrt(20.56)*t)
+plt.plot(t,theta,label="simulation")
+#plt.plot(t,fraserq1_theta,label="answer")
+plt.legend()
 #plt.ylim(0,1)
 
     
